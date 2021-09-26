d5af9d256008ff95454eb9fb8d1d39ee00264459
miunmnn <esteban.principe@utec.edu.pe>
2021-09-25 22:23:04 -0500

    Update seqfile class

--- SeqFile/SequentialFile.h
+++ SeqFile/SequentialFile.h
@@ -1,23 +1,69 @@
 #include<iostream>
 #include<string>
-
+#include<vector>
+#include <algorithm>
 
 struct Registro{
     char codigo[5];
     char nombre[20];
     char carrera[15];
     int ciclo;
-    int next;
-};
+    char next[2]; //Si esta en el main file-> 1m, si esta en el aux->1a
 
+    void setData()
+    {
+        std::cout << "Nombre:";
+        std::cin >> nombre;
+        std::cout << "Carrera: ";
+        std::cin >> carrera;
+        std::cout << "Ciclo: ";
+        std::cin >> ciclo;
+    }
+
+    void showData()
+    {
+        std::cout << "\nCodigo: " <<codigo;
+        std::cout << "\nNombre: " << nombre;
+        std::cout << "\nCarrera: " << carrera;
+        std::cout << "\nCiclo : " << ciclo;
+        std::cout << "\nSgte Registro en:" <<next;
+    }
+
+    std::string getKey()
+    {
+        return nombre;
+    }
+
+};
 
 class SequentialFile{
 private:
     std::string mainFileName;
     std::string auxFileName;
+public:
+    SequentialFile(std::string filename){
+        this->mainFileName=filename+".dat";
+        this->auxFileName="Aux"+filename+".dat";
+    }
 
+
+    void insertAll(std::vector<Registro> registros){
+        for(auto& registro: registros){
+            
+        }
+    };
+
+
+    void add(Registro registr){
+        
+    }
+
+    Registro search(std::string key){
+
+    }
+
+    std::vector<Registro> rangeSearch(std::string begin, std::string end){
+
+    }
 };
-
-
-
 
--- SeqFile/main.cpp
+++ SeqFile/main.cpp
@@ -1,8 +1,80 @@
 #include "SequentialFile.h"
+#include <fstream>
+#include <vector>
+#include <cstring>
+
+//hacerlo lambda
+std::string fill_with_dots(std::string stringval, int size_){
+    while(stringval.size()!=size_){
+        stringval.append(".");
+    }
+    return stringval;
+}
 
 int main(){
-    
+
+    std::string inputFileName="alumnosInputData.txt";
+
+    std::string intFileName(inputFileName);
+    FILE* input_file = fopen(intFileName.c_str(), "r"); 
+
+    std::vector<Registro> registros;
 
 
-    return 0;
+    for(int i = 0 ;i<registros.size() ;i++){
+        std::cout<< registros[i].nombre;
+    }
+    std::string filename="Datos";
+    SequentialFile seqFile(filename);
+
+
+
+
+    return EXIT_SUCCESS;
 }
+
+    //LEER UN TXT E INGRESARLO A UN VECTOR
+
+    // Registro registro;
+        // int atributo=0;
+    // int c;
+    // int iter=-2;
+    // while ((c = fgetc(input_file)) != EOF) {
+    //     if(c==';'){
+    //         switch (atributo)
+    //         {
+    //         case 3:
+    //             iter=0;
+    //             atributo=0;
+    //             registros.push_back(registro);
+    //             continue;
+    //             //break;
+    //         default:
+    //             /* code */
+    //             iter=0;
+    //             atributo++;
+    //             continue;
+    //             //break;
+    //         }
+
+    //     }
+    //     putchar(c);
+    //     switch (atributo)
+    //     {
+    //         case 0:
+    //             registro.codigo[iter]=c;
+    //             break;
+    //         case 1:
+    //             registro.nombre[iter]=c;
+    //             break;
+    //         case 2:
+    //             registro.carrera[iter]=c;
+    //             break;
+    //         default:
+    //             registro.ciclo=c;
+    //             break;
+    //     }
+    //     iter++;
+    //    //putchar(c);
+    // }
+    // fclose(input_file);
